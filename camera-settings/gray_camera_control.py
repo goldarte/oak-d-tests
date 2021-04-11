c2b830624cbbd2e4ad520554fc2a93281a36ddfa
Arthur Golubtsov <goldartt@gmail.com>
2021-04-11 16:37:18 +0300

    Add exposition and iso saving to settings for grey cameras

--- camera-settings/gray_camera_control.py
+++ camera-settings/gray_camera_control.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 
 """
-This example shows usage of Camera Control message as well as camLeftera configInput to change crop x and y
-Uses 'WASD' controls to move the crop window, 'C' to capture a still image, 'T' to trigger autofocus, 'IOKL,.'
-for manual exposure/focus:
+This example shows usage of Camera Control message to change exposure and iso for grey camera
+Uses 'IOKL' for manual exposure/focus:
   Control:      key[dec/inc]  min..max
   exposure time:     I   O      1..33000 [us]
   sensitivity iso:   K   L    100..1600
-  focus:             ,   .      0..255 [far..near]
 To go back to auto controls:
   'E' - autoexposure
-  'F' - autofocus (continuous)
 """
+import os
+import sys
 
 import depthai as dai
 import cv2
 
-# Step size ('W','A','S','D' controls)
-STEP_SIZE = 8
+sys.path.insert(0, os.path.realpath('../'))
+from modules.settings import Settings
+
+settings = Settings("../grey.yaml")
+
+default_settings = {
+    "exp": 1000,
+    "iso": 400
+}
+
+settings.update_defaults(default_settings)
+
 # Manual exposure/focus set step
-EXP_STEP = 500  # us
+EXP_STEP = 100  # us
 ISO_STEP = 50
-LENS_STEP = 3
 
 pipeline = dai.Pipeline()
 
@@ -57,18 +65,17 @@ with dai.Device(pipeline) as dev:
     # Start pipeline
     dev.startPipeline()
 
-    # Defaults and limits for manual focus/exposure controls
-    lensPos = 150
-    lensMin = 0
-    lensMax = 255
-
-    expTime = 20000
+    expTime = settings["exp"]
     expMin = 1
     expMax = 33000
 
-    sensIso = 800
+    sensIso = settings["iso"]
     sensMin = 100
     sensMax = 1600
+
+    ctrl = dai.CameraControl()
+    ctrl.setManualExposure(expTime, sensIso)
+    controlQueue.send(ctrl)
 
     while True:
 
@@ -89,29 +96,10 @@ with dai.Device(pipeline) as dev:
             ctrl = dai.CameraControl()
             ctrl.setCaptureStill(True)
             controlQueue.send(ctrl)
-        elif key == ord('t'):
-            print("Autofocus trigger (and disable continuous)")
-            ctrl = dai.CameraControl()
-            ctrl.setAutoFocusMode(dai.CameraControl.AutoFocusMode.AUTO)
-            ctrl.setAutoFocusTrigger()
-            controlQueue.send(ctrl)
-        elif key == ord('f'):
-            print("Autofocus enable, continuous")
-            ctrl = dai.CameraControl()
-            ctrl.setAutoFocusMode(dai.CameraControl.AutoFocusMode.CONTINUOUS_VIDEO)
-            controlQueue.send(ctrl)
         elif key == ord('e'):
             print("Autoexposure enable")
             ctrl = dai.CameraControl()
             ctrl.setAutoExposureEnable()
-            controlQueue.send(ctrl)
-        elif key in [ord(','), ord('.')]:
-            if key == ord(','): lensPos -= LENS_STEP
-            if key == ord('.'): lensPos += LENS_STEP
-            lensPos = clamp(lensPos, lensMin, lensMax)
-            print("Setting manual focus, lens position:", lensPos)
-            ctrl = dai.CameraControl()
-            ctrl.setManualFocus(lensPos)
             controlQueue.send(ctrl)
         elif key in [ord('i'), ord('o'), ord('k'), ord('l')]:
             if key == ord('i'): expTime -= EXP_STEP
@@ -119,7 +107,9 @@ with dai.Device(pipeline) as dev:
             if key == ord('k'): sensIso -= ISO_STEP
             if key == ord('l'): sensIso += ISO_STEP
             expTime = clamp(expTime, expMin, expMax)
+            settings.update_value("exp", expTime)
             sensIso = clamp(sensIso, sensMin, sensMax)
+            settings.update_value("iso", sensIso)
             print("Setting manual exposure, time:", expTime, "iso:", sensIso)
             ctrl = dai.CameraControl()
             ctrl.setManualExposure(expTime, sensIso)
